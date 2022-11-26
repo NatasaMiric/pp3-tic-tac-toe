13c91e6e32ef5a2a348b821a6d29ab986e677157
NatasaMiric <natasa.miric94@gmail.com>
2022-11-26 18:06:36 +0000

    feat: add source code for displaying the game board

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+board = [' ']*9
+
+
+def display_board(board):
+    """
+    Displays the board.
+
+    Parameters:
+    -----------
+    board : list of int
+        List of numbers from 0 to 8.
+    """
+
+    print(board[0] + '|' + board[1] + '|' + board[2])
+    print('-+-+-')
+    print(board[3] + '|' + board[4] + '|' + board[5])
+    print('-+-+-')
+    print(board[6] + '|' + board[7] + '|' + board[8])
+
+
+display_board(board)
